26d6449e772f510416f5e031ca57757ae88babca
myujin <myujin3927@gmail.com>
2021-11-16 14:55:06 +0900

    Flask 이용한 웹 페이지 연결

---
+++ delisquare/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def basic():
+    return render_template("main.html")
+
+@app.route('/map')
+def map():
+    return render_template("map.html")
+
+@app.route('/search')
+def search():
+    return render_template("search.html")
+
+@app.route('/config')
+def config():
+    return render_template("configuration.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+def create_app():
+    app = Flask(__name__)
+
+    return app
---
+++ delisquare/static/js/loadStore.js
@@ -0,0 +1,27 @@
+$.getJSON("../map_resources/서초구음식점.json",function(store){
+    var data = store;
+    var loc = '';
+    var name = '';
+    //requestOK = true;
+    $.each(data,function(index,val){
+
+        loc=val.전체주소;
+        name = val.업소명;
+
+        displayStore(loc,name,markers);
+    })
+})
+
+$.getJSON("../map_resources/강남구_모범음식점.json",function(store){
+    var data = store;
+    var loc = '';
+    var name = '';
+    //requestOK = true;
+    $.each(data,function(index,val){
+
+        loc=val.소재지지번;
+        name = val.업소명;
+
+        displayStore(loc,name,markers_2);
+    })
+})
