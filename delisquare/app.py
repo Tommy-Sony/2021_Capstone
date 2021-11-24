202a271fc96b9f406034bd57a952f07168cd34a7
myujin <myujin3927@gmail.com>
2021-11-25 02:18:13 +0900

    검색결과 UI 개선

--- delisquare/app.py
+++ delisquare/app.py
@@ -57,22 +57,7 @@ def search_store():
 
 ###############################################
 # 현재있는 파일의 디렉토리 절대경로
-basdir = os.path.abspath(os.path.dirname(__file__))
-# basdir 경로안에 DB파일 만들기
-dbfile = os.path.join(basdir, 'db.sqlite')
 
-# 내가 사용 할 DB URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-# 비지니스 로직이 끝날때 Commit 실행(DB반영)
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# 수정사항에 대한 TRACK
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# SECRET_KEY
-app.config['SECRET_KEY'] = 'jqiowejrojzxcovnklqnweiorjqwoijroi'
-
-db.init_app(app)
-db.app = app
-db.create_all()
 
 ###############################################
 
@@ -80,4 +65,3 @@ if __name__=='__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
     #127.0.0.1
     # debug = True -> 코드 수정할 때마다 Flask가 인식해서 다시 시작
-
